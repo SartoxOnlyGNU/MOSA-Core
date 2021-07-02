f60d5f89dd3394679e7530727c507672224bb101
nifanfa <nifanfa@foxmail.com>
2021-07-02 19:31:21 +0800

    Update

---
+++ Source/Mosa.External/Memory.cs
@@ -0,0 +1,100 @@
+﻿using Mosa.Kernel.x86;
+using Mosa.Runtime;
+using System;
+
+namespace Mosa.Starter.x86
+{
+    static class Memory
+    {
+        public static MemoryBlock GetPhysicalMemory(Pointer address, uint size)
+        {
+            var start = (uint)address.ToInt32();
+
+            for (var at = start; at < start + size; at += PageFrameAllocator.PageSize)
+            {
+                PageTable.MapVirtualAddressToPhysical(at, at);
+            }
+
+            return new MemoryBlock(address, size);
+        }
+    }
+
+	public readonly struct MemoryBlock
+	{
+		private readonly Pointer address;
+		private readonly uint size;
+
+		public Pointer Address { get { return address; } }
+
+		public uint Size { get { return size; } }
+
+		public MemoryBlock(Pointer address, uint size)
+		{
+			this.address = address;
+			this.size = size;
+		}
+
+		private void CheckOffset(uint offset)
+		{
+			if (offset >= size)
+			{
+				throw new ArgumentOutOfRangeException(nameof(offset));
+			}
+		}
+
+		public byte this[uint offset]
+		{
+			get { CheckOffset(offset); return address.Load8(offset); }
+			set { CheckOffset(offset); address.Store8(offset, value); }
+		}
+
+		public byte Read8(uint offset)
+		{
+			CheckOffset(offset);
+			return address.Load8(offset);
+		}
+
+		public void Write8(uint offset, byte value)
+		{
+			CheckOffset(offset);
+			address.Store8(offset, value);
+		}
+
+		public ushort Read16(uint offset)
+		{
+			CheckOffset(offset);
+			return address.Load16(offset);
+		}
+
+		public void Write16(uint offset, ushort value)
+		{
+			CheckOffset(offset);
+			address.Store16(offset, value);
+		}
+
+		public uint Read24(uint offset)
+		{
+			CheckOffset(offset);
+			return address.Load16(offset) | (uint)(address.Load8(offset + 2) << 16);
+		}
+
+		public void Write24(uint offset, uint value)
+		{
+			CheckOffset(offset);
+			address.Store16(offset, (ushort)(value & 0xFFFF));
+			address.Store8(offset + 2, (byte)((value >> 16) & 0xFF));
+		}
+
+		public uint Read32(uint offset)
+		{
+			CheckOffset(offset);
+			return address.Load32(offset);
+		}
+
+		public void Write32(uint offset, uint value)
+		{
+			CheckOffset(offset);
+			address.Store32(offset, value);
+		}
+	}
+}
---
+++ Source/Mosa.External/VMWareSVGAII.cs
@@ -0,0 +1,151 @@
+﻿using Mosa.Kernel.x86;
+using Mosa.Runtime;
+
+namespace Mosa.Starter.x86.Driver
+{
+    public class VMWareSVGAII
+    {
+        public enum Register : ushort
+        {
+            ID = 0,
+            Enable = 1,
+            Width = 2,
+            Height = 3,
+            BitsPerPixel = 7,
+            FrameBufferStart = 13,
+            VRamSize = 15,
+            MemStart = 18,
+            MemSize = 19,
+            ConfigDone = 20,
+            Sync = 21,
+            Busy = 22,
+            FifoNumRegisters = 293
+        }
+
+        private enum ID : uint
+        {
+            Magic = 0x900000,
+            V2 = (Magic << 8) | 2,
+        }
+
+        public enum FIFO : uint
+        {
+            Min = 0,
+            Max = 4,
+            NextCmd = 8,
+            Stop = 12
+        }
+
+        private enum FIFOCommand
+        {
+            Update = 1
+        }
+
+        private enum IOPortOffset : byte
+        {
+            Index = 0,
+            Value = 1,
+        }
+
+        private ushort IndexPort;
+        private ushort ValuePort;
+        public MemoryBlock Video_Memory;
+        private MemoryBlock FIFO_Memory;
+        private PCIDevice device;
+        public uint height;
+        public uint width;
+        public uint depth;
+
+        public VMWareSVGAII()
+        {
+            device = (PCI.GetDevice(VendorID.VMWare, DeviceID.SVGAIIAdapter));
+            device.EnableMemory(true);
+            uint basePort = device.BaseAddressBar[0].BaseAddress;
+            IndexPort = (ushort)(basePort + (uint)IOPortOffset.Index);
+            ValuePort = (ushort)(basePort + (uint)IOPortOffset.Value);
+            WriteRegister(Register.ID, (uint)ID.V2);
+            if (ReadRegister(Register.ID) != (uint)ID.V2)
+                return;
+            Video_Memory = Memory.GetPhysicalMemory(new Pointer(ReadRegister(Register.FrameBufferStart)), ReadRegister(Register.VRamSize));
+            InitializeFIFO();
+        }
+
+        protected void InitializeFIFO()
+        {
+            FIFO_Memory = Memory.GetPhysicalMemory(new Pointer(ReadRegister(Register.MemStart)), ReadRegister(Register.MemSize));
+            FIFO_Memory.Write32((uint)FIFO.Min, (uint)Register.FifoNumRegisters * 4);
+            FIFO_Memory.Write32((uint)FIFO.Max, FIFO_Memory.Size);
+            FIFO_Memory.Write32((uint)FIFO.NextCmd, (uint)FIFO.Min);
+            FIFO_Memory.Write32((uint)FIFO.Stop, FIFO_Memory.Read32((uint)FIFO.Min));
+            WriteRegister(Register.ConfigDone, 1);
+        }
+
+        public void SetMode(uint width, uint height, uint depth = 32)
+        {
+            Disable();
+            this.depth = (depth / 8);
+            this.width = width;
+            this.height = height;
+            WriteRegister(Register.Width, width);
+            WriteRegister(Register.Height, height);
+            WriteRegister(Register.BitsPerPixel, depth);
+            Enable();
+            InitializeFIFO();
+        }
+
+        protected void WriteRegister(Register register, uint value)
+        {
+            IOPort.Out32(IndexPort, (uint)register);
+            IOPort.Out32(ValuePort, value);
+        }
+
+        protected uint ReadRegister(Register register)
+        {
+            IOPort.Out32(IndexPort, (uint)register);
+            return IOPort.In32(ValuePort);
+        }
+
+        protected void SetFIFO(FIFO cmd, uint value)
+        {
+            FIFO_Memory.Write32((uint)cmd, value);
+            return;
+        }
+
+        uint nextcmd = 1172;
+
+        public void Update()
+        {
+            if (nextcmd == 1212) { nextcmd = 1172; }
+
+            SetFIFO((FIFO)(nextcmd), (uint)FIFOCommand.Update);
+            SetFIFO(FIFO.NextCmd, nextcmd + 4);
+            nextcmd += 4;
+
+            SetFIFO((FIFO)(nextcmd), 0);
+            SetFIFO(FIFO.NextCmd, nextcmd + 4);
+            nextcmd += 4;
+
+            SetFIFO((FIFO)(nextcmd), 0);
+            SetFIFO(FIFO.NextCmd, nextcmd + 4);
+            nextcmd += 4;
+
+            SetFIFO((FIFO)(nextcmd), width);
+            SetFIFO(FIFO.NextCmd, nextcmd + 4);
+            nextcmd += 4;
+
+            SetFIFO((FIFO)(nextcmd), height);
+            SetFIFO(FIFO.NextCmd, nextcmd + 4);
+            nextcmd += 4;
+        }
+
+        public void Enable()
+        {
+            WriteRegister(Register.Enable, 1);
+        }
+
+        public void Disable()
+        {
+            WriteRegister(Register.Enable, 0);
+        }
+    }
+}

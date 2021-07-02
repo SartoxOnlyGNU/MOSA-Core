9b2120a939d37fcc95fb19aacdf2ee1f3894a7fa
nifanfa <nifanfa@foxmail.com>
2021-07-02 14:44:33 +0800

    Rewrite

---
+++ Source/Launcher/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Mosa.Compiler.Common.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+		enum Arch
+		{
+			x86,
+			ARMv8A32
+		}
+
+		Settings settings = new Settings();
+
+        public Form1()
+        {
+            InitializeComponent();
+			comboBox1.Items.Add(Arch.x86.ToString());
+			comboBox1.Items.Add(Arch.ARMv8A32.ToString());
+        }
+
+    }
+}

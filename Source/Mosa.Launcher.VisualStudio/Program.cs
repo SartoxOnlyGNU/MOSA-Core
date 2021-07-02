810957c056d63525d4f5179c75e7d31da69f1a81
nifanfa <nifanfa@foxmail.com>
2021-07-02 23:38:26 +0800

    Update

---
+++ Source/Mosa.Launcher.VisualStudio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Mosa.Launcher.VisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Process.Start("Mosa.Launcher.exe", args[0]);
+        }
+    }
+}

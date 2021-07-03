434ffc1c184690b30a8f5a53fdfed84d6e9131dc
nifanfa <nifanfa@foxmail.com>
2021-07-03 12:57:47 +0800

    Update

--- Source/Mosa.Demo.Experiment/Boot.cs
+++ Source/Mosa.Demo.Experiment/Boot.cs
@@ -6,27 +6,25 @@ namespace Mosa.Demo.Experiment
 {
     public static class Boot
     {
-        public static void Main()
+		public static void Main()
+		{
+			Mosa.Kernel.x86.Kernel.Setup();
+
+			IDT.SetInterruptHandler(ProcessInterrupt);
+
+			Screen.Clear();
+			Screen.Goto(0, 0);
+			Screen.Color = ScreenColor.White;
+
+			while (true)
+			{
+				Screen.Goto(0, 0);
+				Screen.Write("This Size:"+sizeof(Boot));
+			}
+		}
+
+        static void ProcessInterrupt(uint interrupt, uint errorCode)
         {
-            Mosa.Kernel.x86.Kernel.Setup();
-
-            IDT.SetInterruptHandler(ProcessInterrupt);
-
-            Screen.Clear();
-            Screen.Goto(0, 0);
-            Screen.Color = ScreenColor.White;
-
-            Program.Setup();
-
-            while (true)
-            {
-                Program.Loop();
-            }
-        }
-
-        public static void ProcessInterrupt(uint interrupt, uint errorCode)
-        {
-            Program.OnInterrupt();
         }
     }
 }

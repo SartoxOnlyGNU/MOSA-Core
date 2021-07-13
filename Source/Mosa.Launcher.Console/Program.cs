8fa36189802dd4b7a792414f8f2a771608c1a88a
nifanfa <nifanfa@foxmail.com>
2021-07-13 19:56:57 +0800

    Update Program.cs

--- Source/Mosa.Launcher.Console/Program.cs
+++ Source/Mosa.Launcher.Console/Program.cs
@@ -241,6 +241,18 @@ namespace Mosa.Launcher.Console
 					TimeSpan timeSpan = DateTime.Now.Subtract(StartTime);
 					System.Console.WriteLine($"Compile End {timeSpan}");
 					break;
+				case CompilerEvent.Exception:
+					System.Console.WriteLine("Fatal! Exception Thrown:");
+					System.Console.WriteLine(message);
+					System.Console.ReadKey();
+					Environment.Exit(0);
+					break;
+				case CompilerEvent.Error:
+					System.Console.WriteLine("Fatal! Compiler Error:");
+					System.Console.WriteLine(message);
+					System.Console.ReadKey();
+					Environment.Exit(0);
+					break;
 			}
 		}
 
